4bb71d2e458f0171b5bb26b69009f1ef604bdef7
Daniel Silveira <danielsilveira.contato@gmail.com>
2020-12-03 20:55:12 -0300

    feat(javascript): Escutando eventos

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -3,33 +3,39 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < pacientes.length; i++){
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-    var paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-    
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-    
-    var tdImc = paciente.querySelector(".info-imc");
-    
-    var pesoEhValido = true;
-    var alturaEhValida = true;
-    
-    if(peso <= 0 || peso >= 1000){
-        console.log("Peso inválido!")
-        pesoEhValido = false;
-        tdImc.textContent = "Peso invállido!"
-    
-    }
-    if(altura <= 0 || altura >= 3.00){
-        console.log("Altura inválida!")
-        alturaEhValida = false;
-        tdImc.textContent = "Altura invállida!"
-    }
-    if(pesoEhValido && alturaEhValida){
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invállido!";
+  }
+  if (altura <= 0 || altura >= 3.0) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura invállida!";
+  }
+  if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
+
+// titulo.addEventListener("click", function () {
+//   console.log("Olha só posso chamar uma função anônima");
+// });
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function () {
+  console.log("Oi, cliquei no botão.");
+});
