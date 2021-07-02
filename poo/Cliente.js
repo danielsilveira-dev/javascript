e4482c5612b6ec04a0e016f3fdd04a05e77dbdd1
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-07-02 15:34:06 -0300

    POO(Conclusao): Parte 1

--- poo/Cliente.js
+++ poo/Cliente.js
@@ -1,4 +1,13 @@
 export class Cliente{
   nome;
-  cpf;
+  _cpf;
+
+  get cpf(){ // Acesso somente só de consulta do cpf
+    return this._cpf;
+  }
+
+  constructor(nome, cpf){
+    this.nome = nome;
+    this._cpf = cpf;
+  }
 }
--- poo/ContaCorrente.js
+++ poo/ContaCorrente.js
@@ -1,42 +1,48 @@
 import { Cliente } from "./Cliente.js";
 
-export class ContaCorrente{
-  agencia;
-  _cliente;
+export class ContaCorrente {
+    static numeroDeContas = 0;
+    agencia;
+    _cliente;
+    _saldo = 0;
 
-  set cliente(novoValor){
-      if(novoValor instanceof Cliente){
-          this._cliente = novoValor;
-      }
-  }
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
 
-  get cliente(){
-      return this._cliente;
-  }
+    get cliente() {
+        return this._cliente;
+    }
 
-   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-  _saldo = 0;
 
-  get saldo(){
-      return this._saldo;
-  }
+    get saldo() {
+        return this._saldo;
+    }
 
-  sacar(valor){
-      if(this._saldo >= valor){
-          this._saldo -= valor;
-          return valor;
-      }
-  }
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
-  depositar(valor){
-      if(valor <= 0){
-          return;
-      } 
-      this._saldo += valor;           
-  }
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
 
-  transferir(valor, conta){
-    const valorSacado = this.sacar(valor);
-    conta.depositar(valorSacado)
-  }
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado)
+    }
 }
