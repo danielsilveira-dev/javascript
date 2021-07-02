4a7513102fe860ea415d27d7b3dc7bcdb686e8b5
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-07-02 10:56:07 -0300

    POO(Modularizando codigo): O que aprendemos?

--- poo/index.js
+++ poo/index.js
@@ -14,12 +14,13 @@ cliente2.cpf = 88822233309;
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1
+contaCorrenteRicardo.depositar(500)
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente()
+conta2.cliente = cliente2
+conta2.agencia = 102
+console.log(conta2);
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-
-console.log(contaCorrenteRicardo);
+contaCorrenteRicardo.transferir(200, conta2)
+console.log(conta2)
+console.log(contaCorrenteRicardo)
