27d82388683e2ac7ec31b7b991c071868c334497
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-07-15 15:40:42 -0300

    Fundamentos do Javascript - Strings e Boolean

---
+++ fundamentos-do-javascript-tipos-variáveis-e-funcoes/type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const meuNome = "Daniel Silveira"
+const citacao = "Meu nome é "
+
+// Concatenação (+)
+
+console.log(`${citacao}${meuNome}`)
