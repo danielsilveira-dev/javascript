dbd32349f911a651ee8905d7d329f2830d14ef1f
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-07-15 17:37:50 -0300

    JavaScript Fundamentos - condiçao ternaria

---
+++ fundamentos-do-javascript-tipos-variáveis-e-funcoes/exercicios/type-boolean-01.js
@@ -0,0 +1,9 @@
+let passou = !true
+passou ? 
+   console.log('Você passou') : 
+   console.log('Verifique suas notas e faça a prova')
+// if(passou == false){
+//    
+// }else{
+//    console.log('Você passou')
+// }
